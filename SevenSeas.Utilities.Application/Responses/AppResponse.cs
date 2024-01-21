a8dd016756d30f13358e570822d3c151fdc7aefa
Huam Benvenutti <huambenvenutti@gmail.com>
2024-01-21 14:49:08 -0300

    app response

---
+++ SevenSeas.Utilities.Application/Responses/AppResponse.cs
@@ -0,0 +1,13 @@
+﻿using SevenSeas.Utilities.Application.Enums;
+using SevenSeas.Utilities.Application.Responses.Interfaces;
+
+namespace SevenSeas.Utilities.Application.Responses;
+
+/// <inheritdoc />
+public class AppResponse<TData> : IAppResponse<TData, AppStatusCode>
+{
+    public bool Success { get; init; }
+    public AppStatusCode AppStatusCode { get; init; }
+    public IList<string>? Errors { get; init; }
+    public TData? Data { get; init; }
+}
---
+++ SevenSeas.Utilities.Application/Responses/Interfaces/IAppResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace SevenSeas.Utilities.Application.Responses.Interfaces;
+
+/// <summary>
+/// Interface for the application response.
+/// </summary>
+/// <typeparam name="TData">The type of the data in the response.</typeparam>
+/// <typeparam name="TStatusCode">The type of the status code in the response.</typeparam>
+public interface IAppResponse<TData, TStatusCode>
+{
+    /// <summary>
+    /// Gets a value indicating whether the response was successful.
+    /// </summary>
+    bool Success { get; init; }
+
+    /// <summary>
+    /// Gets the application status code.
+    /// </summary>
+    TStatusCode AppStatusCode { get; init; }
+
+    /// <summary>
+    /// Gets the list of errors, if any.
+    /// </summary>
+    IList<string>? Errors { get; init; }
+
+    /// <summary>
+    /// Gets the response data.
+    /// </summary>
+    TData? Data { get; init; }
+}
