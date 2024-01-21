8e560079f3dee74deb8a4904f2dcc01cb96e7288
Huam Benvenutti <huambenvenutti@gmail.com>
2024-01-21 16:57:02 -0300

    paged response

--- SevenSeas.Utilities.Application/Responses/AppPaginatedResponse.cs
+++ SevenSeas.Utilities.Application/Responses/AppPaginatedResponse.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using SevenSeas.Utilities.Application.Responses.Interfaces;
+﻿using SevenSeas.Utilities.Application.Responses.Interfaces;
 
 namespace SevenSeas.Utilities.Application.Responses;
 
