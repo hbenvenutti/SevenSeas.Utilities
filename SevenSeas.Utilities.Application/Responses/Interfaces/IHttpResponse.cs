a00dd4caf5fb03133e3bffdd66d1a961701db6ed
Huam Benvenutti <huambenvenutti@gmail.com>
2024-01-21 15:00:25 -0300

    feat http response

--- SevenSeas.Utilities.Application/Responses/Interfaces/IHttpResponse.cs
+++ SevenSeas.Utilities.Application/Responses/Interfaces/IHttpResponse.cs
@@ -3,13 +3,13 @@
 namespace SevenSeas.Utilities.Application.Responses.Interfaces;
 
 /// <summary>
-/// Interface for HTTP responses
+/// Interface for HTTP responses.
 /// </summary>
 
 public interface IHttpResponse
 {
     /// <summary>
-    /// Gets the status code of the HTTP response
+    /// Gets the status code of the HTTP response.
     /// </summary>
-    HttpStatusCode StatusCode { get; init; }
+    HttpStatusCode HttpStatusCode { get; init; }
 }
