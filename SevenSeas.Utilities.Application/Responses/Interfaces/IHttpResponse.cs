c17ee75e91ba28beff5aed34ea38e7c44274da72
Huam Benvenutti <huambenvenutti@gmail.com>
2024-01-21 14:52:24 -0300

    feat: http response interface

---
+++ SevenSeas.Utilities.Application/Responses/Interfaces/IHttpResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace SevenSeas.Utilities.Application.Responses.Interfaces;
+
+/// <summary>
+/// Interface for HTTP responses
+/// </summary>
+
+public interface IHttpResponse
+{
+    /// <summary>
+    /// Gets the status code of the HTTP response
+    /// </summary>
+    HttpStatusCode StatusCode { get; init; }
+}
