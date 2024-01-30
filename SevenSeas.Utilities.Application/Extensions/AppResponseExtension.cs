7c671919a025ef327a4da8a838f96b0b44a1ef32
Huam Benvenutti <huambenvenutti@gmail.com>
2024-01-29 21:08:37 -0300

    http status code

---
+++ SevenSeas.Utilities.Application/Extensions/AppResponseExtension.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using SevenSeas.Utilities.Application.Enums;
+using SevenSeas.Utilities.Application.Responses;
+
+namespace SevenSeas.Utilities.Application.Extensions;
+
+public static class AppResponseExtension
+{
+    public static HttpStatusCode ToHttpStatusCode(this AppResponse response)
+    {
+        switch (response.AppStatusCode)
+        {
+            case AppStatusCode.Success:
+            {
+                return HttpStatusCode.OK;
+            }
+
+            case AppStatusCode.NotFound:
+            {
+                return HttpStatusCode.NotFound;
+            }
+
+            case AppStatusCode.Created:
+            case AppStatusCode.CreatedTransaction:
+            {
+                return HttpStatusCode.Created;
+            }
+
+            case AppStatusCode.ValidationError:
+            {
+                return HttpStatusCode.BadRequest;
+            }
+
+            case AppStatusCode.GatewayTimeout:
+            {
+                return HttpStatusCode.GatewayTimeout;
+            }
+
+            case AppStatusCode.InaccessibleGateway:
+            case AppStatusCode.GatewayAuthenticationError:
+            case AppStatusCode.GatewayAuthorizationError:
+            {
+                return HttpStatusCode.BadGateway;
+            }
+
+            case AppStatusCode.UnexpectedError:
+            default:
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+}
